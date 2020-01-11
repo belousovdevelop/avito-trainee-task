a3b63a2ce83976978cef6efec2cbf07413611522
Roman Belousov <belousovdevelop@gmail.com>
2020-01-11 21:40:42 +0300

    allAdverts contoller complete

--- server/src/controllers/adverts.js
+++ server/src/controllers/adverts.js
@@ -2,10 +2,32 @@ const Ad = require("../models/advert")
 
 class AdsCtrl {
   async allAdverts(req, res) {
+    const { query } = req
+
+    const sort = {}
+    if (query.sortBy && query.orderBy) {
+      sort[query.sortBy] = query.orderBy
+    } else {
+      sort.createdAt = "desc" //  default sort
+    }
+
+    const page = query.page ? parseInt(query.page) : 1
+    const limit = 6 //  adverts per page
+    const skip = limit * (page - 1)
+
+    const conditions = {}
+    const projection = {
+      images: { $slice: 1 }, //  take only first image
+      versionKey: false // disable version key
+    }
+    const options = { sort, limit, skip }
+
     try {
-      const ads = await Ad.find()
+      const ads = await Ad.find(conditions, projection, options)
+      const count = await Ad.countDocuments()
       res.status(200).json({
         status: "success",
+        results: count,
         data: ads
       })
     } catch (err) {
