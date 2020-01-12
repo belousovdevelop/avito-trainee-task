b70b631c9b53ac4e34c1f5f6bb74f1ca1c3640df
Roman Belousov <belousovdevelop@gmail.com>
2020-01-12 18:02:54 +0300

    add validation

--- server/src/models/advert.js
+++ server/src/models/advert.js
@@ -3,19 +3,29 @@ const mongoose = require("mongoose")
 const adSchema = mongoose.Schema(
   {
     title: {
-      type: String
+      type: String,
+      required: [true, "An ad must have a title"],
+      unique: true,
+      maxlength: [200, "An ad title must have less or equal then 200 characters"]
     },
     description: {
-      type: String
+      type: String,
+      required: [true, "An ad must have a description"],
+      trim: true,
+      maxlength: [1000, "An ad description must have less or equal then 1000 characters"]
     },
     address: {
-      type: String
+      type: String,
+      required: [true, "An ad must have a address"],
+      maxlength: [200, "An ad address must have less or equal then 200 characters"]
     },
     price: {
-      type: Number
+      type: Number,
+      required: [true, "An ad must have a price"]
     },
     images: {
-      type: Array
+      type: Array,
+      default: "https://kare.ee/images/no-image.jpg"
     },
     sellerName: {
       type: String,
